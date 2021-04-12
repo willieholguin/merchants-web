419ef2e64abb720aff2d17c41143075e3d2276ff
willieholguin <willieholguin@hotmail.com>
2021-04-12 01:06:20 -0400

    first commit

---
+++ src/test/java/com/hermes/merchantswebapp/MerchantsWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hermes.merchantswebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchantsWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
